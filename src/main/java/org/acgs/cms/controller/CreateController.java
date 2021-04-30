4b5f21a4047f968c1c8e53e46d36c5444d094919
john <tierney-john@hotmail.com>
2021-04-30 22:04:13 +0800

    项目重构

---
+++ src/main/java/org/acgs/cms/controller/CreateController.java
@@ -0,0 +1,12 @@
+package org.acgs.cms.controller;
+
+import org.acgs.autoconfigure.annotation.AcgsBuild;
+import org.acgs.autoconfigure.controller.BuilderController;
+
+/**
+ * @author John@acgs-org
+ * create time 2021/4/30
+ */
+@AcgsBuild
+public class CreateController extends BuilderController {
+}
--- src/test/java/org/acgs/cms/ApplicationTest.java
+++
@@ -1,8 +0,0 @@
-package org.acgs.cms;
-
-/**
- * @Author John
- * @Date 2020/12/16
- */
-public class ApplicationTest {
-}
